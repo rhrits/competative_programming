ec7490938115238a9656151e675b475e09fa6cb0
rhrits <hritik.hritiksingh@gmail.com>
2024-01-12 10:45:30 +0530

    n

---
+++ removingstonegame/removingstone.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maximumScore(int a, int b, int c) {
+    int ans=0;
+    priority_queue<int>q;
+    q.push(a);
+    q.push(b);
+    q.push(c);
+    while(q.size()>1)
+    {
+        int x=q.top();
+        q.pop();
+        int y=q.top();
+        q.pop();
+        x--;
+        y--;
+        if(x){
+            q.push(x);
+        }
+        if(y){
+            q.push(y);
+        }
+        ans++;
+    }
+    return ans;
+}
+
+
+int main(){
+    int a, b , c , ans;
+    cin >> a, b, c ;
+    cout << ans ; 
+}
