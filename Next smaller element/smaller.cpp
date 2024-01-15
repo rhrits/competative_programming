0da0984fb6a1ddbcace971151b0d4e2ae337b2b0
rhrits <hritik.hritiksingh@gmail.com>
2024-01-15 12:27:32 +0530

    n

--- Next smaller element/smaller.cpp
+++ Next smaller element/smaller.cpp
@@ -2,6 +2,27 @@
 #include<iostream>
 
 using namespace std;
+vector<int> output;
+stack<int> s;
+
+for(i=0; i<n; i++){
+    while(s.empty() && stop() >= arr[i]){
+        s.pop();
+    }
+    if(s.empty())
+    {
+        output.push_back(-1);
+    }
+        {
+            else{
+                ans.push_back(stop());
+            }
+        }
+
+    s.push(arr[i]);    
+    }
+
+    
 
 
 
