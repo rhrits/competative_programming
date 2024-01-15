f46d572b3d7a89d7764128f8cfd55a68069478cb
rhrits <hritik.hritiksingh@gmail.com>
2024-01-15 12:47:16 +0530

    n

---
+++ Stock span/stockspan.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void span(int rates[], int days, int stockspan[]){
+
+  stack<int> stack; 
+
+  stockspan[0] = 1;
+  stack.push(0);
+
+  for(int i=1; i<days; i++) 
+  {
+    
+    while(rates[i] > rates[stack.top()])
+    {
+      stack.pop();
+
+      if(stack.empty()) 
+      {
+        break;
+      }
+    }
+
+    
+    if(!stack.empty())
+    {
+      stockspan[i] = i - stack.top();
+    }
+    else
+    {
+      stockspan[i] = i+1;
+    }
+
+    stack.push(i);
+
+  }
+}
+int main() {
+
+  int size = 6;
+  int rates[size] = {31,27,14,21,30,22};
+  int stockspan[size];
+
+  span(rates, size, stockspan);
+
+  for(int i=0; i<size; i++)
+  {
+    cout<< stockspan[i] << ", ";
+  }
+  
+}
+
+ class StockSpanner {
+ public:
+ 	StockSpanner() {}		
+ 	int next(int price) {
+ 		int span = 1;
+ 		while(!stack_.empty() && price >= stack_.top().first){
+ 			span += stack_.top().second;
+ 			stack_.pop();
+ 		}
+ 		stack_.emplace(price, span);
+ 		return span;
+ 	}
+ private:
+ 	stack<pair<int, int>> stack_; 
+ };
