350c2db731a6b19e2dfa8ed30c81b35e5e71a9f1
rhrits <hritik.hritiksingh@gmail.com>
2024-01-15 12:50:48 +0530

    n

--- Stock span/stockspan.cpp
+++ Stock span/stockspan.cpp
@@ -51,18 +51,5 @@ int main() {
   
 }
 
- class StockSpanner {
- public:
- 	StockSpanner() {}		
- 	int next(int price) {
- 		int span = 1;
- 		while(!stack_.empty() && price >= stack_.top().first){
- 			span += stack_.top().second;
- 			stack_.pop();
- 		}
- 		stack_.emplace(price, span);
- 		return span;
- 	}
- private:
- 	stack<pair<int, int>> stack_; 
- };
+ 
+
