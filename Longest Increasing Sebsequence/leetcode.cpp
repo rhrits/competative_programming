a29abd6951858cd8834a132bad12cd59d8e23002
rhrits <hritik.hritiksingh@gmail.com>
2024-02-02 10:21:23 +0530

    n

--- Longest Increasing Sebsequence/leetcode.cpp
+++ Longest Increasing Sebsequence/leetcode.cpp
@@ -14,3 +14,23 @@ public:
         
     }
 };
+
+// O(nlogn) - more optimal solution
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp;
+        for (int i = 0; i < n; ++i)
+        {
+            auto it = lower_bound(dp.begin(), dp.end(), nums[i]);
+            if (it == dp.end())
+                dp.push_back(nums[i]);
+            else
+                *it = nums[i];
+        }
+        return dp.size();    
+        
+    }
+};
